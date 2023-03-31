09fd3a3d2d9f0332e5b02a7b6713c0eee4670d2a
tornikeo <tornikeonoprishvili@gmail.com>
2023-03-31 17:41:48 +0000

    More fixes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from fastapi import status, HTTPException
 from fastapi import Response
 import tempfile
 from pathlib import Path
@@ -17,6 +18,12 @@ async def create_upload_file(file: UploadFile):
     outfile = sess / filepath.with_suffix(".glb")
 
     import bpy
+    import bpy
+
+    objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH')]
+    bpy.ops.object.delete({"selected_objects": objs})
+
+
     if filepath.suffix == '.usdz':
         bpy.ops.wm.usd_import("EXEC_DEFAULT", filepath=str(filepath))
 
@@ -31,13 +38,13 @@ async def create_upload_file(file: UploadFile):
             extract_dir=folder_unpacked,
         )
         print(folder_unpacked)
-        if len(list(folder_unpacked.glob('*.gltf')))>0:
-            gltf_file = list(folder_unpacked.glob('*.gltf'))[0]
+        if len(list(folder_unpacked.glob('**/*.gltf')))>0:
+            gltf_file = list(folder_unpacked.glob('**/*.gltf'))[0]
             print("Detected GLTF, at", folder_unpacked)
             bpy.ops.import_scene.gltf(filepath=str(gltf_file))
             bpy.ops.export_scene.gltf(filepath=str(outfile), export_format='GLB')
-        elif len(list(folder_unpacked.glob('*.obj')))>0:
-            obj_file = list(folder_unpacked.glob('*.obj'))[0]
+        elif len(list(folder_unpacked.glob('**/*.obj')))>0:
+            obj_file = list(folder_unpacked.glob('**/*.obj'))[0]
             bpy.ops.import_scene.obj(
                 filepath=str(obj_file)
             )
@@ -46,7 +53,7 @@ async def create_upload_file(file: UploadFile):
                 export_format="GLB",
             )
         else:
-            return Response(status_code=500)
+            raise HTTPException(detail=f"Provided zip file didn't have gltf or obj files in it {list(folder_unpacked.glob('*'))}.",status_code=500)
     return FileResponse(
         outfile,
         filename=outfile.name,
