5b0b3ec596217cb8463f4aa7962f8177c2940759
luxPavi <pavi.path@luxpmsoft.com>
2023-05-16 17:03:45 -0400

    Move core functionality to seperate service function

--- main.py
+++ main.py
@@ -9,9 +9,23 @@ from pathlib import Path
 from config import get_config
 from ktx2_compress import ktx2_compression
 import bpy
+import requests
+from pydantic import BaseModel
+from service import convert_usdz_upload_glb;
 
 app = FastAPI(debug=True)
 config = get_config()
+
+class UrlRequest(BaseModel):
+    url: str
+
+class UrlToUrlRequest(BaseModel):
+    url: str
+    upload_url: str
+
+class UrlToUrlSplitRequest(BaseModel):
+    url: str
+    upload_url: str
 
 @app.post("/convert")
 async def create_upload_file(file: UploadFile):
@@ -59,11 +73,6 @@ async def create_upload_file(file: UploadFile):
         outfile,
         filename=outfile.name,
     )
-
-import requests
-from pydantic import BaseModel
-class UrlRequest(BaseModel):
-    url: str
 
 @app.post("/convert-from-url")
 async def create_upload_file(request: UrlRequest):
@@ -118,73 +127,20 @@ async def create_upload_file(request: UrlRequest):
         filename=outfile.name,
     )
 
-class UrlToUrlRequest(BaseModel):
-    url: str
-    upload_url: str
-
 @app.post("/convert-from-url-to-url")
 async def create_upload_file(request: UrlToUrlRequest):
 
-    # download file
-    sess = Path(tempfile.mkdtemp())
-    filename = 'upload.usdz'
-    filepath = sess / filename
-    filepath.write_bytes(requests.get(request.url).content)
+    download_url = request.url;
+    upload_url = request.upload_url;
 
-    # contents = await file.read()
-    # filepath = sess / file.filename
-    # filepath.write_bytes(contents)
-    filename = 'upload.glb'
-    outfile = sess / filename
+    return {"success": True, 'upload_response': convert_usdz_upload_glb(download_url, upload_url)}
 
-    bpy.ops.wm.read_factory_settings(use_empty=True)
-    objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH')]
-    bpy.ops.object.delete({"selected_objects": objs})
+@app.post("/convert-payload")
+async def create_upload_file(request: UrlToUrlRequest):
 
-    # TODO: If file id invalid and cannot be open error is logged to console but not thrown
-    if filepath.suffix == '.usdz':
-        bpy.ops.wm.usd_import("EXEC_DEFAULT", filepath=str(filepath))
-    elif filepath.suffix == '.gltf':
-        bpy.ops.import_scene.gltf(filepath=str(filepath))
-    elif filepath.suffix == '.obj':
-        bpy.ops.import_scene.obj(
-            filepath=str(filepath)
-        )
-    elif filepath.suffix == '.zip':
-        folder_unpacked = Path(filepath).with_suffix('')
-        shutil.unpack_archive(
-            filename=filepath,
-            extract_dir=folder_unpacked,
-        )
-        print(folder_unpacked)
-        if len(list(folder_unpacked.glob('**/*.gltf')))>0:
-            gltf_file = list(folder_unpacked.glob('**/*.gltf'))[0]
-            print("Detected GLTF, at", folder_unpacked)
-            bpy.ops.import_scene.gltf(filepath=str(gltf_file))
-        elif len(list(folder_unpacked.glob('**/*.obj')))>0:
-            obj_file = list(folder_unpacked.glob('**/*.obj'))[0]
-            bpy.ops.import_scene.obj(
-                filepath=str(obj_file)
-            )
-        else:
-            raise HTTPException(detail=f"Provided zip file didn't have gltf or obj files in it {list(folder_unpacked.glob('*'))}.",status_code=500)
+    download_url = request.url;
+    upload_url = request.upload_url;
 
-    # TODO: Clear blender to save memory (if needed)
-    bpy.ops.export_scene.gltf(
-        filepath=str(outfile),
-        export_format="GLB",
-        export_draco_mesh_compression_enable=True,
-        export_draco_position_quantization=config.QUANTIZATION
-    )
+    return {"success": True, 'upload_response': convert_usdz_upload_glb(download_url, upload_url)}
 
-    filename = 'upload_compressed.glb'
-    outfile_compressed = sess / filename
-    upload_File = ktx2_compression(outfile, outfile_compressed)
-
-    return {"success": True, 'upload_response': requests.put(
-            request.upload_url, 
-            data=open(upload_File, 'rb'),
-            headers={
-                'Content-Type': 'application/octet-stream',
-            }).status_code}
 
---
+++ service.py
@@ -0,0 +1,83 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
+from fastapi import status, HTTPException
+from fastapi import Response
+import tempfile
+from pathlib import Path
+import shutil, tempfile
+from pathlib import Path
+from config import get_config
+from ktx2_compress import ktx2_compression
+import bpy
+import requests
+from pydantic import BaseModel
+
+config = get_config();
+def convert_usdz_file_to_glb(file_usdz_src, file_glb_dest):
+    bpy.ops.wm.read_factory_settings(use_empty=True)
+    objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH')]
+    bpy.ops.object.delete({"selected_objects": objs})
+
+    # TODO: If file id invalid and cannot be open error is logged to console but not thrown
+    if file_usdz_src.suffix == '.usdz':
+        bpy.ops.wm.usd_import("EXEC_DEFAULT", filepath=str(file_usdz_src))
+    elif file_usdz_src.suffix == '.gltf':
+        bpy.ops.import_scene.gltf(filepath=str(file_usdz_src))
+    elif file_usdz_src.suffix == '.obj':
+        bpy.ops.import_scene.obj(
+            filepath=str(file_usdz_src)
+        )
+    elif file_usdz_src.suffix == '.zip':
+        folder_unpacked = Path(file_usdz_src).with_suffix('')
+        shutil.unpack_archive(
+            filename=file_usdz_src,
+            extract_dir=folder_unpacked,
+        )
+        print(folder_unpacked)
+        if len(list(folder_unpacked.glob('**/*.gltf')))>0:
+            gltf_file = list(folder_unpacked.glob('**/*.gltf'))[0]
+            print("Detected GLTF, at", folder_unpacked)
+            bpy.ops.import_scene.gltf(filepath=str(gltf_file))
+        elif len(list(folder_unpacked.glob('**/*.obj')))>0:
+            obj_file = list(folder_unpacked.glob('**/*.obj'))[0]
+            bpy.ops.import_scene.obj(
+                filepath=str(obj_file)
+            )
+        else:
+            raise HTTPException(detail=f"Provided zip file didn't have gltf or obj files in it {list(folder_unpacked.glob('*'))}.",status_code=500)
+
+    # TODO: Clear blender to save memory (if needed)
+    bpy.ops.export_scene.gltf(
+        filepath=str(file_glb_dest),
+        export_format="GLB",
+        export_draco_mesh_compression_enable=True,
+        export_draco_position_quantization=config.QUANTIZATION
+    )
+
+def convert_usdz_upload_glb(url_download, url_upload):
+    # download file
+    sess = Path(tempfile.mkdtemp())
+    filename = 'upload.usdz'
+    file_usdz = sess / filename
+    file_usdz.write_bytes(requests.get(url_download).content)
+
+    filename = 'upload.glb'
+    file_glb = sess / filename
+
+    # Convert usdz file to glb file
+    convert_usdz_file_to_glb(file_usdz, file_glb);
+
+    # Compress glb file using ktx2
+    filename = 'upload_compressed.glb'
+    file_glb_compressed = sess / filename
+    upload_File = ktx2_compression(file_glb, file_glb_compressed)
+
+    # Upload file
+    upload_result = requests.put(
+        url_upload, 
+        data=open(upload_File, 'rb'),
+        headers={
+            'Content-Type': 'application/octet-stream',
+        });
+    
+    return upload_result.status_code
