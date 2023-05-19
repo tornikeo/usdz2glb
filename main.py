f5d8157781bb4c378f3c1dfe04596ac0ac638b58
luxPavi <pavi.path@luxpmsoft.com>
2023-05-18 21:48:48 -0400

    Add old endpoints in until confirmed they are not used

--- main.py
+++ main.py
@@ -26,14 +26,14 @@ class UrlToUrlQueueRequest(BaseModel):
     to_upload_url: str
     x_access_token: str
     callback_url: str
-    organization_id: str
-    device_name: str
-    lat: str
-    lon: str
+    organization_id: str | None
+    device_name: str | None
+    lat: str | None
+    lon: str | None
     session_id: str
-    scanning_type_id: str
-    subscription_type: str
-    firebase_device_token: str
+    scanning_type_id: str | None
+    subscription_type: str | None
+    firebase_device_token: str | None
     model_name: str
 
 class JobInfoRequest(BaseModel):
@@ -81,3 +81,108 @@ async def create_upload_file(request: JobInfoRequest):
     print(get_job_information(job_id))
 
     return {"success": True, "job_info": get_job_information(job_id).get_status()}
+
+
+# TODO: Confirm if below endpoints are required
+@app.post("/convert")
+async def create_upload_file(file: UploadFile):
+    sess = Path(tempfile.mkdtemp())
+    contents = await file.read()
+    filepath = sess / file.filename
+    filepath.write_bytes(contents)
+    outfile = sess / filepath.with_suffix(".glb")
+
+    objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH')]
+    bpy.ops.object.delete({"selected_objects": objs})
+
+    if filepath.suffix == '.usdz':
+        bpy.ops.wm.usd_import("EXEC_DEFAULT", filepath=str(filepath))
+    elif filepath.suffix == '.gltf':
+        bpy.ops.import_scene.gltf(filepath=str(filepath))
+    elif filepath.suffix == '.obj':
+        bpy.ops.import_scene.obj(
+            filepath=str(filepath)
+        )
+    elif filepath.suffix == '.zip':
+        folder_unpacked = Path(filepath).with_suffix('')
+        shutil.unpack_archive(
+            filename=filepath,
+            extract_dir=folder_unpacked,
+        )
+        print(folder_unpacked)
+        if len(list(folder_unpacked.glob('**/*.gltf')))>0:
+            gltf_file = list(folder_unpacked.glob('**/*.gltf'))[0]
+            print("Detected GLTF, at", folder_unpacked)
+            bpy.ops.import_scene.gltf(filepath=str(gltf_file))
+        elif len(list(folder_unpacked.glob('**/*.obj')))>0:
+            obj_file = list(folder_unpacked.glob('**/*.obj'))[0]
+            bpy.ops.import_scene.obj(
+                filepath=str(obj_file)
+            )
+        else:
+            raise HTTPException(detail=f"Provided zip file didn't have gltf or obj files in it {list(folder_unpacked.glob('*'))}.",status_code=500)
+    
+    bpy.ops.export_scene.gltf(
+        filepath=str(outfile),
+        export_format="GLB",
+    )
+    return FileResponse(
+        outfile,
+        filename=outfile.name,
+    )
+
+class UrlRequest(BaseModel):
+    url: str
+
+@app.post("/convert-from-url")
+async def create_upload_file(request: UrlRequest):
+    # download file
+    sess = Path(tempfile.mkdtemp())
+    filename = 'upload.usdz'
+    filepath = sess / filename
+    filepath.write_bytes(requests.get(request.url).content)
+
+    # contents = await file.read()
+    # filepath = sess / file.filename
+    # filepath.write_bytes(contents)
+    outfile = sess / filepath.with_suffix(".glb")
+
+    bpy.ops.wm.read_factory_settings(use_empty=True)
+    objs = [ob for ob in bpy.context.scene.objects if ob.type in ('CAMERA', 'MESH')]
+    bpy.ops.object.delete({"selected_objects": objs})
+
+    if filepath.suffix == '.usdz':
+        bpy.ops.wm.usd_import("EXEC_DEFAULT", filepath=str(filepath))
+    elif filepath.suffix == '.gltf':
+        bpy.ops.import_scene.gltf(filepath=str(filepath))
+    elif filepath.suffix == '.obj':
+        bpy.ops.import_scene.obj(
+            filepath=str(filepath)
+        )
+    elif filepath.suffix == '.zip':
+        folder_unpacked = Path(filepath).with_suffix('')
+        shutil.unpack_archive(
+            filename=filepath,
+            extract_dir=folder_unpacked,
+        )
+        print(folder_unpacked)
+        if len(list(folder_unpacked.glob('**/*.gltf')))>0:
+            gltf_file = list(folder_unpacked.glob('**/*.gltf'))[0]
+            print("Detected GLTF, at", folder_unpacked)
+            bpy.ops.import_scene.gltf(filepath=str(gltf_file))
+        elif len(list(folder_unpacked.glob('**/*.obj')))>0:
+            obj_file = list(folder_unpacked.glob('**/*.obj'))[0]
+            bpy.ops.import_scene.obj(
+                filepath=str(obj_file)
+            )
+        else:
+            raise HTTPException(detail=f"Provided zip file didn't have gltf or obj files in it {list(folder_unpacked.glob('*'))}.",status_code=500)
+    
+    bpy.ops.export_scene.gltf(
+        filepath=str(outfile),
+        export_format="GLB",
+    )
+    return FileResponse(
+        outfile,
+        filename=outfile.name,
+    )
